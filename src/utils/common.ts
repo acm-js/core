2b5bcfe82f69d3c4a3e82ea79267c728ba7fe900
Alexander Belov <ya-belov@yandex-team.ru>
2020-05-03 18:12:22 +0300

    added mixin decorator

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,26 @@
+/* tslint:disable:ban-types */
 export function isNumber(value: any) {
   return typeof value === 'number' && !Number.isNaN(value);
+}
+
+export function mixins<T extends { new (...args: any[]) }>(constructor: T) {
+  return (ctors: any[]) => {
+    ctors.forEach(ctor => {
+      Object.getOwnPropertyNames(ctor.prototype).forEach(name => {
+        Object.defineProperty(
+          constructor.prototype,
+          name,
+          Object.getOwnPropertyDescriptor(ctor.prototype, name)
+        );
+      });
+
+      Object.getOwnPropertySymbols(ctor.prototype).forEach(symbol => {
+        Object.defineProperty(
+          constructor.prototype,
+          symbol,
+          Object.getOwnPropertyDescriptor(ctor.prototype, symbol)
+        );
+      });
+    });
+  }
 }
