422bb25e5714ce477374a67f19d7044e77346fd5
Alexander Belov <ya-belov@yandex-team.ru>
2020-05-04 03:38:58 +0300

    chore(update): new changes

--- src/lib/inherits.ts
+++ src/lib/inherits.ts
@@ -1,4 +1,4 @@
-export function inherit(derivedCtor: any, baseCtors: any[]) {
+export function inherits(derivedCtor: any, baseCtors: any[]) {
   baseCtors.forEach(baseCtor => {
     const names = [
       ...Object.getOwnPropertyNames(baseCtor.prototype),
