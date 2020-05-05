377ae784a894f84898c9a729b50c0fc1c62d68af
Alexander Belov <ipritoflex@yandex.ru>
2020-05-05 15:11:05 +0300

    chore(update): new changes

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,9 @@
+import crypto from 'crypto';
+
 export function isNumber(value: any) {
   return typeof value === 'number' && !Number.isNaN(value);
+}
+
+export function generateKey(bufferSize = 16, encoding = 'hex'): string {
+  return crypto.randomBytes(bufferSize).toString(encoding);
 }
